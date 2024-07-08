a18dcd5d68723f16e2e40ef1bb6584c2dd7010fa
gustavo <gustavogarciaci1@gmail.com>
2024-07-08 20:53:44 -0300

    chore: initial node setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import fastify from "fastify"
+
+const app = fastify()
+
+app.get("/test", () => {
+  return "Hello World!"
+})
+
+app.listen({ port: 3333 }).then(() => console.log("HTTP server is running"))

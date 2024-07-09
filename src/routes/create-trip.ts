5be67f37f1a4287affc811b6bdcf222de62ea3af
gustavo <gustavogarciaci1@gmail.com>
2024-07-09 16:04:26 -0300

    feat: inviting users when creating trip

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -18,7 +18,8 @@ export async function createTrip(app: FastifyInstance) {
           starts_at: z.coerce.date(),
           ends_at: z.coerce.date(),
           owner_name: z.string(),
-          owner_email: z.string().email()
+          owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email())
         }),
         response: {
           201: z.object({
@@ -28,7 +29,7 @@ export async function createTrip(app: FastifyInstance) {
       }
     },
     async (req, reply) => {
-      const { destination, ends_at, starts_at,owner_email, owner_name } = req.body
+      const { destination, ends_at, starts_at,owner_email, owner_name, emails_to_invite } = req.body
 
       if(dayjs(starts_at).isBefore(new Date())) {
         throw new Error("The trip cannot start in the past")
@@ -42,7 +43,22 @@ export async function createTrip(app: FastifyInstance) {
         data: {
           destination,
           starts_at,
-          ends_at
+          ends_at,
+          participants: {
+            createMany: {
+              data: [
+                {
+                  email: owner_email,
+                  name: owner_name,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return { email }
+                })
+              ]
+            }
+          }
         }
       })
 

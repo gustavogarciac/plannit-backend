667a603f332371ed2d5898054959d985cd5781e8
gustavo <gustavogarciaci1@gmail.com>
2024-07-09 15:50:45 -0300

    feat: nodemailer setup

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -3,6 +3,8 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod"
 import dayjs from "dayjs"
+import { getMailClient } from "@/lib/mailer";
+import nodemailer from "nodemailer"
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,7 +16,9 @@ export async function createTrip(app: FastifyInstance) {
         body: z.object({
           destination: z.string().min(4),
           starts_at: z.coerce.date(),
-          ends_at: z.coerce.date()
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email()
         }),
         response: {
           201: z.object({
@@ -24,7 +28,7 @@ export async function createTrip(app: FastifyInstance) {
       }
     },
     async (req, reply) => {
-      const { destination, ends_at, starts_at } = req.body
+      const { destination, ends_at, starts_at,owner_email, owner_name } = req.body
 
       if(dayjs(starts_at).isBefore(new Date())) {
         throw new Error("The trip cannot start in the past")
@@ -42,6 +46,22 @@ export async function createTrip(app: FastifyInstance) {
         }
       })
 
+      const mail = await getMailClient()
+
+      const message = await mail.sendMail({
+        from: {
+          name: "Plann.it team",
+          address: "attending@plannit.com",
+        },
+        to: {
+          name: owner_name,
+          address: owner_email
+        },
+        subject: "Your trip has been created",
+        html: `<p>Your has trip has been successfully created. You can access the details of it and invite new users clicking here.</p>`
+      })
+
+      console.log(nodemailer.getTestMessageUrl(message))
 
       return {
         tripId: trip.id

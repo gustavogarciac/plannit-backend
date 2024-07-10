0caa38ceec1864004838c22085a68f406013c44c
gustavo <gustavogarciaci1@gmail.com>
2024-07-10 15:54:00 -0300

    feat: get activities route

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { dayjs } from "@/lib/dayjs"
 
 export async function getActivities(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -16,10 +17,13 @@ export async function getActivities(app: FastifyInstance) {
         response: {
           201: z.object({
             activities: z.array(z.object({
-              id: z.string().uuid(),
-              title: z.string(),
-              occurs_at: z.date(),
-              trip_id: z.string().uuid()
+              date: z.date(),
+              activities: z.array(z.object({
+                id: z.string().uuid(),
+                title: z.string(),
+                occurs_at: z.date(),
+                trip_id: z.string().uuid()
+              }))
             }))
           })
         }
@@ -33,13 +37,28 @@ export async function getActivities(app: FastifyInstance) {
           id: tripId
         },
         include: {
-          activities: true
+          activities: {
+            orderBy: {
+              occurs_at: 'asc'
+            }
+          }
         }
       })
 
       if(!trip) throw new Error("Trip not found")
 
-      return reply.status(201).send({ activities: trip.activities })
+      const differenceInDaysBetweenTripStartsAndEnd = dayjs(trip.ends_at).diff(dayjs(trip.starts_at), 'days')
+
+      const activities = Array.from({ length: differenceInDaysBetweenTripStartsAndEnd + 1 }).map((_, index) => {
+        const date = dayjs(trip.starts_at).add(index, 'days')
+
+        return {
+          date: date.toDate(),
+          activities: trip.activities.filter(activity => dayjs(activity.occurs_at).isSame(date, 'day'))
+        }
+      })
+
+      return reply.status(201).send({ activities })
     }
   )
 }
